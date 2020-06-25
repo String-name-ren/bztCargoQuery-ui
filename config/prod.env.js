844ad7691dc08ed819c48709b6c72b589400399f
renph <308129771@qq.com>
2020-06-25 21:57:07 +0800

    init

---
+++ config/prod.env.js
@@ -0,0 +1,5 @@
+module.exports = {
+    NODE_ENV: '"production"',
+    BASE_API: '"/bztCargoQuery"',
+    APP_ORIGIN: '"https://www.xapp.com"'
+};
---
+++ config/test.env.js
@@ -0,0 +1,5 @@
+module.exports = {
+    NODE_ENV: '"test"',
+    BASE_API: '"/bztCargoQuery"',
+    APP_ORIGIN: '"https://www.xapp.com"'
+};
